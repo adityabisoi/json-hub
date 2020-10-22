33d4ba1b967e4bb97f92b49ae604ea3029c1e385
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-10-22 18:57:48 +0530

    error handling added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,31 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
+import morgan from 'morgan'
 const app: Application = express()
+
 const usersRoute = require('./api/routes/users')
 
-app.use('/users',usersRoute)
+app.use(morgan('dev'))
 
-module.exports=app
+// Routes to handle requests
+app.use('/users', usersRoute)
+
+// Handle error
+interface ErrorWithStatus extends Error {
+    status: number
+}
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new Error('Not found') as ErrorWithStatus
+    error.status = 404
+    next(error)
+})
+
+app.use((error: ErrorWithStatus, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app
