438fb14997b6472faa488c5b477c5adabe13bde7
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-10-22 10:32:34 +0530

    typescript with base configuration added

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-const greet: string = 'Hello World'
+import express from 'express'
+
+const app=express()
+const port:number=3000
+
+const greet: string = "Hello World";
+
+app.get('/',(req,res)=>{
+    res.send(greet)
+})
+
+app.listen(port,()=>console.log(`Server running on ${port}`))
