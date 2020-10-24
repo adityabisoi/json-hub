c4181da06248fec82c71ab8679c717e53950a311
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-10-24 07:46:29 +0530

    users POST with id added

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -1,8 +1,8 @@
 import express, { Application, Request, Response, NextFunction } from 'express'
-import  mongoose  from 'mongoose'
+import mongoose from 'mongoose'
 const router = express.Router()
 
-const User=require('../routes/user')
+const User = require('../routes/user')
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({
@@ -11,22 +11,16 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 })
 
 router.post('/', (req: Request, res: Response, next: NextFunction) => {
-    // type User = {
-    //     id: number
-    //     first_name: string
-    //     last_name: string
-    //     email: string
-    // }
-    const user=new User({
+    const user = new User({
         _id: new mongoose.Types.ObjectId(),
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         email: req.body.email
     })
-    
+
     user.save()
-        .then((res: string)=>console.log(res))
-        .catch((err: string)=>console.log(err))
+        .then((data: string) => console.log(data))
+        .catch((err: string) => console.log(err))
 
     res.status(201).json({
         msg: 'Users POST',
@@ -37,19 +31,19 @@ router.post('/', (req: Request, res: Response, next: NextFunction) => {
 router.get('/:userId', (req: Request, res: Response, next: NextFunction) => {
     const id: string = req.params.userId
 
-    // Single user valid
-    if (id == 'valid') {
-        res.status(200).json({
-            msg: 'Single user valid'
+    User.findById(id).exec()
+        // Single user valid
+        .then((data: any) => {
+            console.log(data)
+            res.status(200).json(data)
         })
-    }
-
-    // Single user invalid
-    else {
-        res.status(200).json({
-            msg: 'Single user invalid'
+        // Single user invalid
+        .catch((err: any) => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
         })
-    }
 })
 
 module.exports = router
