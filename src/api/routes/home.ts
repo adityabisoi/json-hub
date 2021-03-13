e320a7648596c643f6709e3e1e53813e76554026
Aditya Bisoi <adityabisoi1999@gmail.com>
2021-03-13 08:36:49 +0530

    get URL on dev and prod

--- src/api/routes/home.ts
+++ src/api/routes/home.ts
@@ -2,7 +2,8 @@ import express, { Application, Request, Response, NextFunction } from 'express'
 const router = express.Router()
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('GET (Valid): https://rest-fetch.herokuapp.com/users/ ')
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+    res.send(`GET (Valid): ${fullUrl}users`)
 })
 
 module.exports = router
