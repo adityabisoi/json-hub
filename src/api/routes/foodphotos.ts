1ae250253b503f49796310901567252cd9d576dd
chiragasawa <chiragasawa1@gmail.com>
2021-03-31 21:14:09 +0530

    api response updated

--- src/api/routes/foodphotos.ts
+++ src/api/routes/foodphotos.ts
@@ -19,7 +19,7 @@ const router = express.Router();
 // GET request /photos/food/taste route -> available category
 router.get("/taste", (req: Request, res: Response, next: NextFunction) => {
   try {
-    res.status(200).json(data.taste);
+    res.status(200).json({ data: data.taste });
   } catch (err) {
     //error handling
     console.log(err);
@@ -34,7 +34,7 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
   try {
     //if query parameters does not exist then send all food items
     if (!req.query.taste) {
-      res.status(200).json(data.data);
+      res.status(200).json({ data: data.data });
     }
     // If query parameters exist then sending that type of food object
     else {
@@ -44,7 +44,7 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
           sendData.push(element);
         }
       });
-      res.status(200).json(sendData);
+      res.status(200).json({ data: sendData });
     }
   } catch (err) {
     //error handling
