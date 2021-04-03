79eff7f53b3c7935e7d950ab93419a9231c8e0d8
Ritik <ritikpr307@gmail.com>
2021-04-03 21:55:40 +0530

    added PATCH req in users route

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -1,78 +1,90 @@
-import express, { Application, Request, Response, NextFunction } from "express";
-import mongoose from "mongoose";
-const router = express.Router();
-const dateFormat = require("dateformat");
+import express, { Application, Request, Response, NextFunction } from 'express'
+import mongoose from 'mongoose'
+const router = express.Router()
+var dateFormat = require('dateformat');
 
-const User = require("../models/user");
+const User = require('../models/user')
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
     User.find().exec()
         .then((data: string) => {
-            console.log(data);
-            res.status(200).json(data);
+            console.log(data)
+            res.status(200).json(data)
             
         })
         .catch((err: string) => {
-            console.log(err);
+            console.log(err)
             res.status(500).json({
-                error: err,
-            });
-        });
-});
+                error: err
+            })
+        })
+})
 
-router.get("/:userId", (req: Request, res: Response, next: NextFunction) => {
-    const id: string = req.params.userId;
+router.get('/:userId', (req: Request, res: Response, next: NextFunction) => {
+    const id: string = req.params.userId
 
     User.findById(id).exec()
         // Single user valid
         .then((data: string) => {
-            console.log(data);
+            console.log(data)
             if (data) {
                 res.status(200).json({
-                    data: data,
-                });
+                    data: data
+                })
             }
             else {
                 res.status(404).json({
-                    message: "Id does not exist",
-                });
+                    message: 'Id does not exist'
+                })
             }
         })
         // Single user invalid
         .catch((err: string) => {
-            console.log(err);
+            console.log(err)
             res.status(500).json({
-                error: err,
-            });
-        });
-});
+                error: err
+            })
+        })
+})
 
-router.patch("/", (req: Request, res: Response, next: NextFunction) => {
-    const jsonData=req.body;
-    jsonData["timestamp"]=dateFormat(new Date(), "yyyy-mm-dd HH-MM");
+router.patch('/:id', (req: Request, res: Response, next: NextFunction) => {
+    let jsonData=req.body;
     
-    res.status(200).json(jsonData);
-});
+    // jsonData['timestamp']=dateFormat(new Date(), "yyyy-mm-dd HH-MM")
+    User.findById(req.params.id).exec().then((user:any)=>{
+        
+        let {first_name=user.first_name,last_name=user.last_name,email=user.email}=jsonData;
+        
+        res.status(200).json({first_name:first_name,
+            last_name:last_name,
+            email:email
+        });
+    })
+    .catch((err:string)=>{
+        res.status(404).json({message:"user not found"});
+    })
+    
+})
 
-router.delete("/:userId", (req: Request, res: Response, next: NextFunction) => {
+router.delete('/:userId', (req: Request, res: Response, next: NextFunction) => {
     const id: any = req.params.userId;
     User.findById(id).exec()
         .then((data: string) => {
             if (data) {
-                res.status(204).json();
+                res.status(204).json()
             }
             else {
                 res.status(404).json({
-                    message: "User ID does not exist",
-                });
+                    message: 'User ID does not exist'
+                })
             }
         })
         .catch((err: string) => {
-            console.log(err);
+            console.log(err)
             res.status(500).json({
-                error: err,
-            });
-        });
-});
+                error: err
+            })
+        })
+})
 
 module.exports = router;
