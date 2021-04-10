1b8a95c35e7bc12b2f06e82eebc9410a848e78ef
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-04-11 00:49:05 +0530

    Added extended endpoint for vehicles

--- src/api/routes/vehicles.ts
+++ src/api/routes/vehicles.ts
@@ -8,7 +8,33 @@ const router = express.Router();
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).json(data);
+        const manufacturer: any = req.query.manufacturer;
+        const result = [];
+        if (manufacturer !== undefined) {
+            for (let i: any = 0; i < data.data.length; i++) {
+                if (data.data[i].manufacturer === manufacturer) {
+                    result.push(data.data[i]);
+                }
+            }
+            res.status(200).json({ data: result });
+        }
+        else {
+            res.status(200).json(data);
+        }
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+router.get("/:index", (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const idx: number = parseInt(req.params.index);
+        if (idx < data.data.length) {
+            res.status(200).json({ data: data.data[idx] });
+        }
+        else {
+            res.status(200).json(data);
+        }
     } catch (err) {
         res.status(500).json({ error: err });
     }
