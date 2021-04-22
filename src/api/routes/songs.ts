101300e2e56986af6e37b5c1e4d2c7eefc0ced1f
Ritik <ritikpr307@gmail.com>
2021-04-22 20:32:29 +0530

    made requested changes

--- src/api/routes/songs.ts
+++ src/api/routes/songs.ts
@@ -3,42 +3,41 @@ const data = require("../../../data/songdata.json");
 
 const router = express.Router();
 
-const fetchData=(key:string,value:string)=>{
-    let result:any=[];
-    data.data.forEach(function(song:any){
-        if(song[key]===value){
-            result.push(song);
+const fetchData = (key:string,value:string)=>{
+    const result:any = data.data.filter(function (song:any){
+        if(song[key] === value){
+            return song;
         }
     });
     return result;
-}
+};
 
 
 // GET request /songs
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
-        const artist:any=req.query.artist;
-        const genre:any=req.query.genre;
-        const language:any=req.query.language;
+        const artist:any = req.query.artist;
+        const genre:any = req.query.genre;
+        const language:any = req.query.language;
         if(req.query){
             if(artist){
                 const result = fetchData("singer",String(artist));
                 if(result.length)
-                    res.status(200).json({data:result});
+                    res.status(200).json({ data:result });
                 else
                     res.status(404).json({ message: "Invalid artist" });    
             }
             else if(genre){
                 const result = fetchData("genre",String(genre));
                 if(result.length)
-                    res.status(200).json({data:result});
+                    res.status(200).json({ data:result });
                 else
                     res.status(404).json({ message: "Invalid genre" });
             }
             else if(language){
                 const result = fetchData("language",String(language));
                 if(result.length)
-                    res.status(200).json({data:result});
+                    res.status(200).json({ data:result });
                 else
                     res.status(404).json({ message: "Invalid language" });
             }
