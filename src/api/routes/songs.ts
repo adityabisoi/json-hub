612371215e7a02b79597da7e8c2ec64c8a30a43e
Ritik Rawal <ritikpr307@gmail.com>
2021-04-24 20:55:06 +0530

    Added endpoints in songs route (#105)
    * added enpoints in songs route
    
    * made requested changes
    
    * remove function and added single lined arrow func
    
    * added reqested changes
    
    * lint the code
    
    * added space before typed annotations
    
    * Update songs.ts

--- src/api/routes/songs.ts
+++ src/api/routes/songs.ts
@@ -6,7 +6,26 @@ const router = express.Router();
 // GET request /songs
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).json(data);
+        if (req.query) {
+            const artist: any = req.query.artist;
+            const genre: any = req.query.genre;
+            const language: any = req.query.language;
+            if (!artist && !genre && !language) {
+                res.status(404).json({ message: "Invalid query data" });
+            } else {
+                const result = data.data
+                    .filter((song: any) => song.singer === (artist ?? song.singer))
+                    .filter((song: any) => song.genre === (genre ?? song.genre))
+                    .filter((song: any) => song.language === (language ?? song.language));
+
+                if (result.length) 
+                    res.status(200).json({ data: result });
+                else 
+                    res.status(204).json({ message: "No record found" });
+            }
+        } else {
+            res.status(200).json(data);
+        }
     } catch (err) {
         console.log(err);
         res.status(500).json({
