542bb2ad8a24d3bdc4db5f49b7abac1ebb86c0f2
Ritik <ritikpr307@gmail.com>
2021-04-24 10:41:34 +0530

    added space before typed annotations

--- src/api/routes/songs.ts
+++ src/api/routes/songs.ts
@@ -7,9 +7,9 @@ const router = express.Router();
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
         if (req.query) {
-            const artist:any = req.query.artist;
-            const genre:any = req.query.genre;
-            const language:any = req.query.language;
+            const artist:  any = req.query.artist;
+            const genre: any = req.query.genre;
+            const language: any = req.query.language;
             if (!artist && !genre && !language) {
                 res.status(404).json({ message: "Invalid query data" });
             } else {
