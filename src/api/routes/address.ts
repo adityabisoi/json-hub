80493edce10df369e85a6c8e0e5de5e18b5a29df
Samriddhi <Agrawalsamriddhi83@gmail.com>
2021-04-24 08:47:48 +0530

    Extended address endpoint (#106)
    * Extended address endpoint
    
    * minor bug fix
    
    * minor bug fix
    
    * Fixed filter chaining
    
    * minor changes(const and status code)

--- src/api/routes/address.ts
+++ src/api/routes/address.ts
@@ -6,8 +6,26 @@ const router = express.Router();
 //GET request /address route
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
-        console.log(data);
-        res.status(200).json(data);
+        const city: any = req.query.city;
+        const state: any = req.query.state;
+        const country: any = req.query.country;
+        if (Object.keys(req.query).length) {
+            if (!city && !state && !country)
+                res.status(404).json({ message: "Invalid query" });
+            else {
+                const result = data.data
+                    .filter((address: any) => address.city === (city ?? address.city))
+                    .filter((address: any) => address.state === (state ?? address.state))
+                    .filter((address: any) => address.country === (country ?? address.country));
+
+                if (result.length)
+                    res.status(200).json({ data: result });
+                else
+                    res.status(204).json({ message: "No data found" });
+            }
+        } else {
+            res.status(200).json(data);
+        }
     }
     catch (err) {
         console.log(err);
