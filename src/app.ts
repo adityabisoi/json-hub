611035480cabb8a7c91b7bd154d6b9abf215e5eb
Samriddhi <Agrawalsamriddhi83@gmail.com>
2021-04-26 13:58:26 +0530

    Added shows route (#111)

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ const photoRoute = require("./api/routes/photos");
 const foodPhotoRoute = require("./api/routes/foodphotos"); //include foodphotos route
 const vehicleRoute = require("./api/routes/vehicles"); //include vehicles route
 const songRoute = require("./api/routes/songs"); //include songs route
+const showsRoute = require("./api/routes/shows"); //include shows route
 
 const options = {
     useNewUrlParser: true,
@@ -72,11 +73,9 @@ app.use("/comments", commentRoute);//Routing the app to use the comments endpoin
 app.use("/photos/food", foodPhotoRoute); //Added foodphoto route
 app.use("/photos", photoRoute);
 app.use("/vehicles", vehicleRoute); //Added vehicles route
-
 app.use("/animals", animalRoute);
-
 app.use("/songs", songRoute);
-
+app.use("/shows", showsRoute);
 
 app.use("/animals", animalRoute);   //Added animal route
 app.use("/address", addressRoute);   //Added address route
