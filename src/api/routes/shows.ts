cb42eccc70a627aa2e141ec05a58ae3f55fa1b19
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-04-26 12:10:13 +0530

    Added shows route

---
+++ src/api/routes/shows.ts
@@ -0,0 +1,38 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+const data = require("../../../data/showsdata.json");
+
+const router = express.Router();
+
+//GET request /shows route
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.status(200).json(data);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+        });
+    }
+});
+
+//GET request/shows/:id route
+router.get("/:id", (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id: any = req.params.id;
+        if (id < data.data.length) {
+            res.status(200).json({ data: data.data[id] });
+        }
+        else {
+            res.status(404).json({ message: "No data found at the given index" });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err,
+        });
+    }
+});
+
+module.exports = router;
