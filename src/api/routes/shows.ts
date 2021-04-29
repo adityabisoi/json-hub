c06ce4f369bf2f5be626ec1b9457fa3f6aac1369
Samriddhi <Agrawalsamriddhi83@gmail.com>
2021-04-29 08:26:25 +0530

    Extended endpoints (#118)

--- src/api/routes/shows.ts
+++ src/api/routes/shows.ts
@@ -6,7 +6,28 @@ const router = express.Router();
 //GET request /shows route
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
-        res.status(200).json(data);
+        const genre: any = req.query.genre;
+        const season: any = req.query.season;
+        const rating: any = req.query.rating;
+        if (Object.keys(req.query).length) {
+            if (!genre && !season && !rating)
+                res.status(404).json({ message: "Invalid query" });
+            else {
+                let result = data.data
+                    .filter((shows: any) => shows.season === (season ?? shows.season))
+                    .filter((shows: any) => shows.rating === (rating ?? shows.rating))
+                if (genre) result = data.data.filter((shows: any) => shows.genre.includes(genre))
+
+                if (result.length)
+                    res.status(200).json({ data: result });
+                else
+                    res.status(204).json({ message: "No data found" });
+            }
+        }
+        else {
+            res.status(200).json(data);
+        }
+
     }
     catch (err) {
         console.log(err);
