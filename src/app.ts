4861bdf135971af82edca9a99530cea13d885a15
Samriddhi <Agrawalsamriddhi83@gmail.com>
2021-05-01 23:56:51 +0530

    Added movie route (#123)
    * Added movie route
    
    * Updated lowercasing
    
    * Updated genre as a list
    
    * Update moviedata.json
    
    * Update moviedata.json

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,7 @@ const vehicleRoute = require("./api/routes/vehicles"); //include vehicles route
 const songRoute = require("./api/routes/songs"); //include songs route
 const bookRoute = require("./api/routes/books"); //include books route
 const showsRoute = require("./api/routes/shows"); //include shows route
+const movieRoute = require("./api/routes/movies");
 const productRoute = require("./api/routes/products"); //include products route
 
 const options = {
@@ -82,8 +83,8 @@ app.use("/shows", showsRoute);
 app.use("/animals", animalRoute);   //Added animal route
 app.use("/address", addressRoute);   //Added address route
 app.use("/books", bookRoute);   //Added books route
+app.use("/movies", movieRoute);
 app.use("/products", productRoute);   //Added products route
-
 
 // Handle error
 interface ErrorWithStatus extends Error {
