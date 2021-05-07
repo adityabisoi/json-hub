fc03a97a708b9239ae5dbb685ae5d0002f42ebb9
Samridhi Agrawal <agrawalsamriddhi83@gmail.com>
2021-05-07 17:33:54 +0530

    Extended sports route

--- src/api/routes/sports.ts
+++ src/api/routes/sports.ts
@@ -20,11 +20,30 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
 router.get("/:category", (req: Request, res: Response, next: NextFunction) => {
     try {
         const category: any = req.params.category;
-        const result = data.data.filter((sport: any) => sport.category === category);
-        if (result.length)
-            res.status(200).json(result);
-        else
-            res.status(404).json({ message: "Invalid category" });
+        const player: any = req.query.players;
+        const result = data.data.filter((sport: any) => sport.category === category)[0];
+        if (result && Object.keys(req.query).length) {
+            if (!player)
+                res.status(404).json({ message: "Invalid query" });
+            else {
+                const ans: any = [];
+                const length = result.players.length;
+                for (let i: any = 0; i < length; i++) {
+                    if (result.players[i].name === player)
+                        ans.push(result.players[i]);
+                }
+                if (ans.length)
+                    res.status(200).json({ data: ans });
+                else
+                    res.status(204).json({ message: "No data found" });
+            }
+        }
+        else {
+            if (result)
+                res.status(200).json({ data: result });
+            else
+                res.status(404).json({ message: "Invalid category" });
+        }
     }
     catch (err) {
         console.log(err);
