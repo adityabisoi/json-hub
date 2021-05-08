246b69c85bab338fde54d82af1838c31d638d890
Ritik Rawal <ritikpr307@gmail.com>
2021-05-08 16:11:14 +0530

    Extended animals route (#138)
    * updated user route
    
    * extended animal route
    
    * fixed requested changes
    
    * made the requested changes

--- src/api/routes/animals.ts
+++ src/api/routes/animals.ts
@@ -7,25 +7,38 @@ const router = express.Router();
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(200).json(data);
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
         res.status(500).json({
             error: err,
         });
-    } 
+    }
 });
 
 router.get("/:category", (req: Request, res: Response, next: NextFunction) => {
-    const category:any = req.params.category ;
-    const result = data.data.filter((animal:any)=>animal.category === category);
+    const category: any = req.params.category;
+    let result = data.data.find((animal: any) => animal.category === category);
     try {
-        if(result.length)
-            res.status(200).json(result);
-        else
-            res.status(404).json({ message: "Invalid category" });    
-    }
-    catch (err) {
+        if (result && Object.keys(req.query).length) {
+            const type: any = req.query.type;
+            if (!type) {
+                res.status(404).json({ message: "Invalid query" });
+            } else {
+                result = result.types.filter((animal: any) => animal.type === type);
+                if (result.length) {
+                    res.status(200).json(result);
+                } else {
+                    res.status(204).json({ message: "No data found" });
+                }
+            }
+        } else {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).json({ message: "Invalid category" });
+            }
+        }
+    } catch (err) {
         console.log(err);
         res.status(500).json({
             error: err,
