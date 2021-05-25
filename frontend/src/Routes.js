271702de9579a1c6bdf4b5339b8e475332b14bae
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-25 16:41:38 +0530

    created homepage and added content about existing routes

---
+++ frontend/src/Routes.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+ 
+class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ROUTES</h2>
+        <p>Here are the API routes fetched in this project</p>
+        <ol>
+          <li>address routes</li>
+          <li>animals routes</li>
+          <li>books routes</li>
+          <li>comments routes</li>
+          <li>foodphotos routes</li>
+          <li>home routes</li>
+          <li>movies routes</li>
+          <li>photos routes</li>
+          <li>products routes</li>
+          <li>shows routes</li>
+          <li>songs routes</li>
+          <li>sports routes</li>
+          <li>tasks routes</li>
+          <li>users routes</li>
+          <li>usertest routes</li>
+          <li>vehicles routes</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Routes;
