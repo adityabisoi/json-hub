de02987c58538cfc2c5ab6dfb459ae9d0e565157
Subhangi Choudhary <53938490+subhangi2731@users.noreply.github.com>
2021-05-26 20:27:17 +0530

    created homepage and added content about existing routes (#151)
    * created homepage and added content

---
+++ frontend/src/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>REST-FETCH PROJECT</h2>
+        <p>A community-owned REST API service for testers and developers.
+        Fetch provides REST API endpoints for different types of placeholders,
+        which can be easily used during testing and development without the
+        need for creating sample data manually.</p>
+ 
+        <p>The endpoint usertest which follows the same schema
+        as the users/ endpoint, must be used for testing</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
