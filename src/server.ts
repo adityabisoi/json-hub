d05c2a9158596b5e3e6f641b39065f9cc89b07dc
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-30 22:52:56 +0530

    changed port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import http from "http";
 const app = require("./app");
 
 // Define port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(port);
