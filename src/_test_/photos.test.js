b37928be5b583b17fab918420ff3432518709dbc
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-31 23:26:09 +0530

    added tests for photos endpoint

---
+++ src/_test_/photos.test.js
@@ -0,0 +1,21 @@
+const supertest = require('supertest');
+const endpoint = 'http://localhost:5000/photos'
+
+describe("Testing the photos endpoint", () => {
+
+    it("pPhotos route returns true for status", async () => {
+
+        const response = await supertest(endpoint).get('/');
+        expect(response.status).toBe(200);
+
+    });
+
+    it("Tests the photos route with an individual ID", async () => {
+
+        const response = await supertest(endpoint).get('/5/');
+        expect(response.status).toBe(200);
+        expect(response.body.data.name).toBe('yellow son robot')
+        expect(response.body.data.picture).toBe('https://robohash.org/6')
+    });
+
+});
