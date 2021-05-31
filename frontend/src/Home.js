a7b86abcecd1f36ffaac037d29f7a42b8c95f8d7
Subhangi Choudhary <53938490+subhangi2731@users.noreply.github.com>
2021-05-31 19:19:32 +0530

    added tests for home component (#172)

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div data-testid="home-div">
         <h2>REST-FETCH PROJECT</h2>
         <p>A community-owned REST API service for testers and developers.
         Fetch provides REST API endpoints for different types of placeholders,
