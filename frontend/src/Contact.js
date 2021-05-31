7fb7d864bfa24d8371468ebaac0189d2c25a5205
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-05-31 18:18:46 +0530

    added tests for contact component

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Contact extends Component {
   render() {
     return (
-      <div>
+      <div data-testid="contact-div">
         <h2>GOT QUESTIONS?</h2>
         <p>The easiest thing to do is post on
         our <a href="https://discord.com/channels/811724426965811210/815033344407896135">discord Channel</a>.
