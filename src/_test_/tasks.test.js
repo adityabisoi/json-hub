fe3bc6516074b8430843295386c0e652f7394c32
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2021-06-02 18:13:24 +0530

    added tests for tasks endpoint (#180)

---
+++ src/_test_/tasks.test.js
@@ -0,0 +1,21 @@
+const supertest = require('supertest');
+const endpoint = 'http://localhost:5000/tasks'
+
+describe("Testing the tasks endpoint", () => {
+
+    it("Tasks route returns true for status", async () => {
+
+        const response = await supertest(endpoint).get('/');
+        expect(response.status).toBe(200);
+
+    });
+
+    it("Tests the tasks route with an individual ID", async () => {
+
+        const response = await supertest(endpoint).get('/1/');
+        expect(response.status).toBe(200);
+        expect(response.body.data.task_name).toBe('drinking')
+        expect(response.body.data.details).toBe('drink delicious chai or coffee')
+    });
+
+});
